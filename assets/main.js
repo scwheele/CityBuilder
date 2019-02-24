d65913b838ec596eb791aeb2265910e7d849f8df
Scott Wheeler <scott_wheeler@outlook.com>
2019-02-24 12:48:14 -0800

    Added game logic, can now upgrade nodes.

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,58 @@
 var game = {
-    totalCash: 0,
+    totalCash: 10,
     currentCPS: 0
 }
 
 var levels = new Map();
-
 var ticks = 0;
+var msgTimeout;
+
+function clearMessage() {
+    document.getElementById('messages').innerHTML = "";
+}
+
+function addMessage(message) {
+    clearInterval(msgTimeout);
+    document.getElementById('messages').innerHTML = message;
+    msgTimeout = setTimeout(clearMessage, 3000);
+}
+
+function updateObject(name) {
+    if(name != null) {
+        console.log(name);
+        console.log(levels.get(name));
+        if(levels.get(name) == null) {
+            return console.error('Game object does not exist');
+        }
+
+        let level = levels.get(name);
+
+        if(level.unlocked == false) {
+            var neededCash = (level.cps * 10);
+            console.log("Needed cash: ", neededCash);
+            if(game.totalCash >= neededCash) {
+                level.unlocked = true;
+                document.getElementById(level.name + '-upgrade-button').innerHTML = "Upgrade";
+                game.totalCash = game.totalCash - neededCash;
+            }else{
+                addMessage("You don't have enough cash for that. Amount needed: " + neededCash + "!");
+            }
+        }else{
+            var neededCash = (level.cps * 5);
+            console.log("Needed cash: ", neededCash);
+            if(game.totalCash >= neededCash) {
+                level.level++;
+                level.cps = (level.baseCPS * level.level);
+                game.totalCash = game.totalCash - neededCash;
+            }else{
+                addMessage("You don't have enough cash for that. Amount needed: " + neededCash + "!");
+            }
+        }
+
+    }else{
+        return console.error('No name provided.');
+    }
+}
 
 function gameLoop() {
     ticks++;
@@ -13,7 +60,7 @@ function gameLoop() {
 
     levels.forEach(level => {
         if(level.unlocked) {
-            let cps = (level.cps * level.level);
+            let cps = (level.baseCPS * level.level);
             game.totalCash += cps;
             game.currentCPS += cps;
 
@@ -31,16 +78,16 @@ function gameLoop() {
 
 function main() {
     // create levels
-    levels.set('Cave', { name: 'Cave', level: 1, cps: 1, unlocked: true });
-    levels.set('Hut', { name: 'Hut', level: 1, cps: 5, unlocked: false });
-    levels.set('Cabin', { name: 'Cabin', level: 1, cps: 10, unlocked: false });
-    levels.set('Apartment', { name: 'Apartment', level: 20, cps: 1, unlocked: false });
-    levels.set('Condo', { name: 'Condo', level: 1, cps: 40, unlocked: false });
-    levels.set('SmallHouse', { name: 'SmallHouse', level: 80, cps: 1, unlocked: false });
-    levels.set('MediumHouse', { name: 'MediumHouse', level: 160, cps: 1, unlocked: false });
-    levels.set('LargeHouse', { name: 'LargeHouse', level: 320, cps: 1, unlocked: false });
-    levels.set('Mansion', { name: 'Mansion', level: 1, cps: 640, unlocked: false });
-    levels.set('Palace', { name: 'Palace', level: 1, cps: 1280, unlocked: false });
+    levels.set('Cave', { name: 'Cave', level: 1, cps: 1, baseCPS: 1, unlocked: false });
+    levels.set('Hut', { name: 'Hut', level: 1, cps: 5, baseCPS: 5, unlocked: false });
+    levels.set('Cabin', { name: 'Cabin', level: 1, cps: 10, baseCPS: 10, unlocked: false });
+    levels.set('Apartment', { name: 'Apartment', level: 1, cps: 20, baseCPS: 20, unlocked: false });
+    levels.set('Condo', { name: 'Condo', level: 1, cps: 40, baseCPS: 40, unlocked: false });
+    levels.set('SmallHouse', { name: 'SmallHouse', level: 1, cps: 80, baseCPS: 80, unlocked: false });
+    levels.set('MediumHouse', { name: 'MediumHouse', level: 1, cps: 160, baseCPS: 160, unlocked: false });
+    levels.set('LargeHouse', { name: 'LargeHouse', level: 1, cps: 320, baseCPS: 320, unlocked: false });
+    levels.set('Mansion', { name: 'Mansion', level: 1, cps: 640, baseCPS: 640, unlocked: false });
+    levels.set('Palace', { name: 'Palace', level: 1, cps: 1280, baseCPS: 1280, unlocked: false });
 
     // render levels
     levels.forEach(level => {
@@ -52,6 +99,12 @@ function main() {
         var newUL = document.createElement("ul");
         newUL.setAttribute('id', level.name + "-attrib-list");
         newSection.appendChild(newUL);
+
+        var button = document.createElement("button");
+        button.setAttribute('onclick', 'updateObject(\'' + (level.name).toString() + '\')');
+        button.setAttribute('id', level.name + "-upgrade-button");
+        button.innerHTML = "Unlock";
+        newSection.appendChild(button);
 
         for(var attrib in level) {
             var node = document.createElement("li");
